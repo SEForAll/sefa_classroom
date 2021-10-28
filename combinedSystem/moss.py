284ac6d9d6d77231eedc345b8cdff419ab971364
Daniel Chun <dchun@purdue.edu>
2021-10-28 13:36:05 -0400

    more comments

--- combinedSystem/moss.py
+++ combinedSystem/moss.py
@@ -31,7 +31,6 @@ outputFile.write(datetime.now().strftime("%m-%d %H:%M:%S") + "\n")
 #!!----------Set Up Command Line Flag Input--------!!
 parser = argparse.ArgumentParser("specify run options")
 group = parser.add_mutually_exclusive_group()
-group.add_argument("--hw_range", type = str, nargs = 2, help = "specify a range of homeworks to grade. example: python3 runSystem.py --hw_range hw02sort hw04file")
 group.add_argument("--grade_all", action="store_true", help = "specify this option to grade all homeworks. example: python3 runSystem.py --grade_all")
 parser.add_argument("--config", type = str, nargs = 1, help = "specify the absolute path of a config.json file")
 parser.add_argument("--git_username", type = str, nargs = 1, help = "grade only a specific student's homework")
@@ -50,6 +49,8 @@ authKey = configInputs["authKey"] #json file
 
 #!!----------Run Actual System--------!!
 [students, hws, repos] = fetchLists(fetchRepos(organization, authName, authKey), gitUser)  #fetchRepos returns json file of repos, then fetchLists returns list of students in class and lists of homeworks that exist
+
+#!!----------Clone Repos in TempMossDirectory----------!!
 os.mkdir("TempMossDirectory")
 os.chdir("TempMossDirectory")
 print(students)
@@ -57,30 +58,32 @@ for student in students:
     cloneCmd = "git clone git@github.com:{}/{}-{}".format(organization, hw_number, student)
     os.system(cloneCmd)
 
+#!!----------Directory Work----------!!
 path = os.getcwd()
 temppath = os.getcwd() # to get into the Moss Directory
-#print(path)
 baseDirectory = path.replace("/TempMossDirectory", "")
 os.chdir(baseDirectory)
 
+#!!----------Move Specific Files to MossDirectory----------!!
 for root, dir, files in os.walk("TempMossDirectory"):
     for file in files:
-        if ".c" in file: # change this
+        if ".c" in file: # CHANGE THIS if you want to run different types of files
             for student in students:
                 if student in root:
                     shutil.copy(os.path.join(root, file), os.path.join("MossDirectory", hw_number + "_" + student + ".c"))
 
+#!!----------Run Moss Script----------!!
 toMossDirectory = temppath.replace("/TempMossDirectory", "/MossDirectory")
 os.chdir(toMossDirectory)
+runMoss = "perl moss.pl -l c *.c" # CHANGE THIS if you want to change the paramaters of the script, 
+subprocess.call(runMoss, shell = True) # ^ look at the script email for more details
 
-runMoss = "perl moss.pl -l c *.c"
-subprocess.call(runMoss, shell = True)
-
+#!!----------Delete TempMossDirectory----------!!
 os.chdir(baseDirectory)
-
 deleteTempMossDirectory = "rm -rf ./TempMossDirectory"
 os.system(deleteTempMossDirectory)
 
+#!!----------Delete Other Files in MossDirectory----------!!
 dirs = os.listdir(toMossDirectory)
 os.chdir(toMossDirectory)
 for file in dirs: 
